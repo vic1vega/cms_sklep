a5a55de26d892ce88a85f3da50954652dc3717e7
vic1vega <janusz.pacholski@outlook.com>
2018-10-24 20:46:52 +0200

    Data Trandsfer Object for pages

---
+++ Sklep_CMS/Areas/Admin/Controllers/PagesController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Sklep_CMS.Areas.Admin.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Admin/Pages
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Sklep_CMS/Models/Data/PageDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sklep_CMS.Models.Data
+{
+    [Table("tblPages")]
+    public class PageDTO
+    {
+        public int Id { get; set; }
+        public string Title{ get; set; }
+        public string Slug { get; set; }
+        public string Body{ get; set; }
+        public string Sorting { get; set; }
+        public bool HasSidebar { get; set; }
+
+
+    }
+}
