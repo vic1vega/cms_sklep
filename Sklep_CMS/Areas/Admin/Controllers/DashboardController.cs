698ad011662019f7460ee7f8ceb7f72667a450c1
vic1vega <janusz.pacholski@outlook.com>
2018-10-24 15:38:06 +0200

    Areas Admin modification (front end + merge bundle)

---
+++ Sklep_CMS/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Sklep_CMS.Areas.Admin.Controllers
+{
+    public class DashboardController : Controller
+    {
+        // GET: Admin/Dashboard
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
