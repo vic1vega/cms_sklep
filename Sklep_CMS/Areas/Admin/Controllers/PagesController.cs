0979ffba490ee1cc23db38e7fcf7e93d3048081a
vic1vega <janusz.pacholski@outlook.com>
2018-10-25 14:28:17 +0200

    Admin Pages (part 4 - adding add page: ending)

--- Sklep_CMS/Areas/Admin/Controllers/PagesController.cs
+++ Sklep_CMS/Areas/Admin/Controllers/PagesController.cs
@@ -24,9 +24,62 @@ namespace Sklep_CMS.Areas.Admin.Controllers
         }
 
         // GET: Admin/Pages/AddPage
+        [HttpGet]
         public ActionResult AddPage()
         {
             return View();
         }
+
+        // POST: Admin/Pages/AddPage
+        [HttpPost]
+        public ActionResult AddPage(PageVM model)
+        {
+            //form model state
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            using (CmsDataBase db = new CmsDataBase())
+            {
+
+                string slug;
+                
+                //initalization PageDTO
+                PageDTO dto = new PageDTO();
+
+                //slug is Empty - assign title to slug
+                if (string.IsNullOrWhiteSpace(model.Slug))
+                {
+                    slug = model.Title.Replace(" ", "-").ToLower();
+                }
+                else
+                {
+                    slug = model.Slug.Replace(" ", "-").ToLower();
+                }
+
+                //this same slug exsist
+                if (db.Pages.Any(x => x.Title == model.Title) || db.Pages.Any(x => x.Slug == slug))
+                {
+                    ModelState.AddModelError(key: "", errorMessage: "Tytuł tworzonej strony lub jej adres już istnieje w sklepie");
+                    return View(model);
+                }
+
+                dto.Title = model.Title;
+                dto.Slug = slug;
+                dto.Body = model.Body;
+                dto.HasSidebar = model.HasSidebar;
+                dto.Sorting = 1000;
+
+                //save DTO to DB
+                db.Pages.Add(dto);
+                db.SaveChanges();
+            }
+
+            TempData["SM"] = "Dodałeś nową stronę";
+
+            return RedirectToAction("AddPage");
+        }
     }
+
 }
